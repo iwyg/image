5851b546764e7058c8c3a2bb105a47c7decb65a8
iwyg <mail@thomas-appel.com>
2015-01-19 13:49:52 +0100

    dont call pear install on hhvm

---
+++ .travis.install.sh
@@ -0,0 +1,11 @@
+set -x
+if [ "$TRAVIS_PHP_VERSION" = 'hhvm' ] || [ "$TRAVIS_PHP_VERSION" = 'hhvm-nightly' ] ; then
+    echo "skipping imagick installation"
+else
+	pear config-set preferred_state beta
+	printf "\n" | pecl install imagick
+	echo "extension = imagick.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+	php -m | grep imagick 
+	composer self-update
+	composer install --prefer-source --no-interaction --dev
+fi
