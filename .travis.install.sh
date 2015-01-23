686f45dfcaecae2d200fb545a044c73d1be0d765
iwyg <mail@thomas-appel.com>
2015-01-23 21:40:04 +0100

    fix shell script

--- .travis.install.sh
+++ .travis.install.sh
@@ -3,14 +3,14 @@ sudo apt-get remove -y imagemagick libmagickcore-dev libmagickwand-dev
 sudo apt-get autoremove
 sudo apt-get install -y libtiff-dev libjpeg-dev libdjvulibre-dev libwmf-dev pkg-config
 
-if ["$IMAGE_DRIVER" = "imagick"] ; then
+if [ "$IMAGE_DRIVER" = "imagick" ] ; then
 	pear config-set preferred_state beta
 	printf "\n" | pecl install imagick
 	echo "extension = imagick.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 	php --ri imagick 
 fi
 
-if ["$IMAGE_DRIVER" = "gmagick"] ; then
+if [ "$IMAGE_DRIVER" = "gmagick" ] ; then
 	pear config-set preferred_state beta
 	printf "\n" | pecl install gmagick
 	echo "extension=gmagick.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
