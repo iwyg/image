74cb43a5422703be8b6925e6dd71ec7ea70f234e
iwyg <mail@thomas-appel.com>
2015-01-24 02:29:45 +0100

    the heck

--- .travis.install.sh
+++ .travis.install.sh
@@ -5,15 +5,15 @@ sudo apt-get install -y libtiff-dev libjpeg-dev libdjvulibre-dev libwmf-dev pkg-
 
 if [ "$IMAGE_DRIVER" = "imagick" ] ; then
 	pear config-set preferred_state beta
-	sudo pecl install imagick-3.2.0RC1
-	echo \"extension=imagick.so\" >> `php --ini | grep \"Loaded Configuration\" | sed -e \"s|.*:\s*||\"`;
+	pecl install imagick-3.2.0RC1
+	echo "extension=imagick.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`;
 	php --ri imagick;
 fi
 
 if [ "$IMAGE_DRIVER" = "gmagick" ] ; then
 	pear config-set preferred_state beta
-	sudo pecl install gmagick-1.1.7RC2
-	echo \"extension=gmagick.so\" >> `php --ini | grep \"Loaded Configuration\" | sed -e \"s|.*:\s*||\"`;
+	pecl install gmagick-1.1.7RC2
+	echo "extension=gmagick.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`;
 	php --ri gmagick;
 fi
 
