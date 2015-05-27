a3215a5ba8fd85c58fa430089dbe708cacc745b8
iwyg <mail@thomas-appel.com>
2015-05-27 09:42:40 +0200

    only remove imagemagick for imagick

--- .travis.install.sh
+++ .travis.install.sh
@@ -1,13 +1,14 @@
 composer self-update
 composer -vvv install --prefer-source --no-interaction --dev
 
-sudo apt-get remove -y imagemagick libmagickcore-dev libmagickwand-dev;
-sudo apt-get autoremove;
-sudo apt-get install -y libtiff-dev libjpeg-dev libpng-dev libdjvulibre-dev libwmf-dev pkg-config;
-
 if [ "$IMAGE_DRIVER" = "imagick" ] ; then
+
+	sudo apt-get remove -y imagemagick libmagickcore-dev libmagickwand-dev;
+	sudo apt-get autoremove;
+	sudo apt-get install -y libtiff-dev libjpeg-dev libpng-dev libdjvulibre-dev libwmf-dev pkg-config;
 	curl -O http://www.imagemagick.org/download/releases/ImageMagick-6.9.0-4.tar.gz;
 	tar xzf ImageMagick-6.9.0-4.tar.gz;
+
 	cd ImageMagick-6.9.0-4;
 	./configure --prefix=/usr/local/imagemagick;
 	make -j;
@@ -26,22 +27,5 @@ if [ "$IMAGE_DRIVER" = "imagick" ] ; then
 	php --ri imagick;
 	cd ..;
 fi
-
-if [ "$IMAGE_DRIVER" = "im" ] ; then
-	sudo apt-get install -y libtiff-dev libjpeg-dev imagemagick;
-fi
-
-#if [ "$IMAGE_DRIVER" = "gmagick" ] ; then
-#	curl -O http://pecl.php.net/get/gmagick-1.1.7RC2.tgz;
-#	tar xzf gmagick-1.1.7RC2.tgz;
-#	cd gmagick-1.1.7RC2;
-#	phpize;
-#	./configure --with-gmagick=/usr/local;
-#	make -j;
-#	sudo make install;
-#	echo "extension = imagick.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-#	php --ri gmagick;
-#	cd ..;
-#fi
 
 export TEST_RUNNING_IN_CI=true;
