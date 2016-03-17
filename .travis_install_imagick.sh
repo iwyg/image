bb130a929483bee8264adf658eb9b827717a167b
iwyg <mail@thomas-appel.com>
2016-03-17 14:09:47 +0100

    test if we can install imagemagick stuff

---
+++ .travis_install_imagick.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+sudo install -y libmagickcore-dev libmagickwand-dev imagemagick
+
+git clone https://github.com/mkoppanen/imagick.git
+
+cd imagick
+phpize
+./configure --with-php-config=`which php-config`
+sudo make
+sudo make install
+
+PHP_VERSION = `phpenv version-name`;
+echo "extension = imagick.so" >> ~/.phpenv/versions/$PHP_VERSION/etc/php.ini
+php --ri imagick;
+cd ..
